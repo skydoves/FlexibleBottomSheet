cbb12272a4974b9cbde8df6a62b1b2172aee9ccb
skydoves <skydoves@naver.com>
2023-11-29 16:19:06 +0900

    Update apiValidation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,69 +16,69 @@ import com.skydoves.flexible.Configuration
 
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id(libs.plugins.android.application.get().pluginId)
-    id(libs.plugins.kotlin.android.get().pluginId)
+  id(libs.plugins.android.application.get().pluginId)
+  id(libs.plugins.kotlin.android.get().pluginId)
 }
 
 android {
-    compileSdk = Configuration.compileSdk
-    namespace = "com.skydoves.flexiblebottomsheetdemo"
-    defaultConfig {
-        applicationId = "com.skydoves.flexiblebottomsheetdemo"
-        minSdk = Configuration.minSdk
-        targetSdk = Configuration.targetSdk
-        versionCode = Configuration.versionCode
-        versionName = Configuration.versionName
-    }
+  compileSdk = Configuration.compileSdk
+  namespace = "com.skydoves.flexiblebottomsheetdemo"
+  defaultConfig {
+    applicationId = "com.skydoves.flexiblebottomsheetdemo"
+    minSdk = Configuration.minSdk
+    targetSdk = Configuration.targetSdk
+    versionCode = Configuration.versionCode
+    versionName = Configuration.versionName
+  }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
-    }
+  composeOptions {
+    kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
+  }
 
-    kotlinOptions {
-        jvmTarget = libs.versions.jvmTarget.get()
-    }
+  kotlinOptions {
+    jvmTarget = libs.versions.jvmTarget.get()
+  }
 
-    buildFeatures {
-        compose = true
-        buildConfig = true
-    }
+  buildFeatures {
+    compose = true
+    buildConfig = true
+  }
 
-    packaging {
-        resources {
-            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
-        }
+  packaging {
+    resources {
+      excludes.add("/META-INF/{AL2.0,LGPL2.1}")
     }
+  }
 
-    lint {
-        abortOnError = false
-    }
+  lint {
+    abortOnError = false
+  }
 }
 
 dependencies {
-    implementation(project(":flexible-bottomsheet-material"))
-    implementation(project(":flexible-bottomsheet-material3"))
+  implementation(project(":flexible-bottomsheet-material"))
+  implementation(project(":flexible-bottomsheet-material3"))
 
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling)
-    implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.compose.runtime)
-    implementation(libs.androidx.compose.material)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.constraintlayout)
-    implementation(libs.androidx.compose.material.iconsExtended)
-    implementation(libs.androidx.compose.navigation)
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.compose.ui)
+  implementation(libs.androidx.compose.ui.tooling)
+  implementation(libs.androidx.compose.foundation)
+  implementation(libs.androidx.compose.runtime)
+  implementation(libs.androidx.compose.material)
+  implementation(libs.androidx.compose.material3)
+  implementation(libs.androidx.compose.constraintlayout)
+  implementation(libs.androidx.compose.material.iconsExtended)
+  implementation(libs.androidx.compose.navigation)
 
-    implementation(libs.landscapist.glide)
-    implementation(libs.landscapist.placeholder)
-    implementation(libs.landscapist.animation)
+  implementation(libs.landscapist.glide)
+  implementation(libs.landscapist.placeholder)
+  implementation(libs.landscapist.animation)
 
-  implementation("com.github.skydoves:orbital:0.3.2")
+  implementation(libs.orbital)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -14,42 +14,47 @@
 
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.jetbrains.compose) apply false
-    alias(libs.plugins.baseline.profile) apply false
-    alias(libs.plugins.kotlin.binary.compatibility)
-    alias(libs.plugins.nexus.plugin)
-    alias(libs.plugins.spotless)
-    alias(libs.plugins.dokka)
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.kotlin.multiplatform) apply false
+  alias(libs.plugins.jetbrains.compose) apply false
+  alias(libs.plugins.baseline.profile) apply false
+  alias(libs.plugins.kotlin.binary.compatibility)
+  alias(libs.plugins.nexus.plugin)
+  alias(libs.plugins.spotless)
+  alias(libs.plugins.dokka)
   alias(libs.plugins.kotlin.android) apply false
 }
 
-subprojects {
-    apply(plugin = rootProject.libs.plugins.spotless.get().pluginId)
+apiValidation {
+  ignoredProjects.addAll(listOf("app", "baselineprofile-app"))
+  nonPublicMarkers.add("com.skydoves.flexible.core.InternalFlexibleApi")
+}
 
-    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-        kotlin {
-            target("**/*.kt")
-            targetExclude("$buildDir/**/*.kt")
-            ktlint().editorConfigOverride(
-                mapOf(
-                    "indent_size" to "2",
-                    "continuation_indent_size" to "2"
-                )
-            )
-            licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
-            trimTrailingWhitespace()
-            endWithNewline()
-        }
-        format("xml") {
-            target("**/*.xml")
-            targetExclude("**/build/**/*.xml")
-            // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
-            licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
-            trimTrailingWhitespace()
-            endWithNewline()
-        }
+subprojects {
+  apply(plugin = rootProject.libs.plugins.spotless.get().pluginId)
+
+  configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    kotlin {
+      target("**/*.kt")
+      targetExclude("$buildDir/**/*.kt")
+      ktlint().editorConfigOverride(
+        mapOf(
+          "indent_size" to "2",
+          "continuation_indent_size" to "2"
+        )
+      )
+      licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
+      trimTrailingWhitespace()
+      endWithNewline()
     }
+    format("xml") {
+      target("**/*.xml")
+      targetExclude("**/build/**/*.xml")
+      // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
+      licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
+      trimTrailingWhitespace()
+      endWithNewline()
+    }
+  }
 }
