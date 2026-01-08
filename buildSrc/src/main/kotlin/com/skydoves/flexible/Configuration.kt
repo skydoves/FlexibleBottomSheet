fc79b03a860c5f9d11ab7726bf883be92299a9f9
skydoves <skydoves@naver.com>
2026-01-08 11:25:24 +0900

    Prepare for release 0.2.0

--- buildSrc/src/main/kotlin/com/skydoves/flexible/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/flexible/Configuration.kt
@@ -5,10 +5,10 @@ object Configuration {
   const val targetSdk = 36
   const val minSdk = 23
   const val majorVersion = 0
-  const val minorVersion = 1
-  const val patchVersion = 6
+  const val minorVersion = 2
+  const val patchVersion = 0
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 7
+  const val versionCode = 8
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
